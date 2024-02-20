7b8b8ecfc816d717a6f1eacab5e6d0e1aa56d121
Decea Cezara <deceacezara2003@gmail.com>
2024-02-20 22:09:11 +0200

    finished

--- src/main/java/com/example/laborator7gui/FriendRequestsApplication.java
+++ src/main/java/com/example/laborator7gui/FriendRequestsApplication.java
@@ -12,7 +12,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class FriendRequestsApplication extends Application {
